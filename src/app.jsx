d5bd45c1d30f92e48594bd0ca2df6ad5390bdc55
xgbbing <xgb.xu@qq.com>
2018-05-11 13:42:26 +0800

    dd

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './index.scss';
+ReactDOM.render(
+	<div>
+		<h1>111</h1>
+	</div>,
+  document.getElementById('app')
+);
